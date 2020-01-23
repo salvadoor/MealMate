fa4f650cdf4ccf06ac71698db23c7074fdd75a22
salvador <46605004+salvadoor@users.noreply.github.com>
2020-01-22 18:31:51 -0600

    General Layout for Settings, no real functionality yet

--- app/src/main/java/com/srg/mealmate/SettingsFragment.java
+++ app/src/main/java/com/srg/mealmate/SettingsFragment.java
@@ -8,13 +8,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SettingsFragment extends Fragment {
-
+    private FirebaseUser user;
+    private View view;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -24,8 +29,12 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        view = inflater.inflate(R.layout.fragment_settings, container, false);
+        TextView emailTV = view.findViewById(R.id.user_email);
+        emailTV.setText(user.getEmail());
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        return view;
     }
 
 }
